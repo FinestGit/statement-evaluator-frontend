f55334d3e42a93771e1f869b2492f5d93ff4bf18
Chase Rogness <chaserogness@gmail.com>
2024-10-18 10:36:40 -0500

    Added in some docs for readFile

--- src/utils/ReadFile.ts
+++ src/utils/ReadFile.ts
@@ -1,3 +1,8 @@
+/**
+ * Takes in a File and returns a Promise that contains the files contents when the file has finished being read.
+ * @param {File} file - The file that we are expecting to be read
+ * @returns {Promise<string | null>} The promise that contains either a null or a string with the files contents.
+ */
 export const readFile = (file: File): Promise<string | null> => {
     return new Promise((resolve, reject) => {
         const fileReader = new FileReader();
